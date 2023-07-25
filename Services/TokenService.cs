ed7c93443aae8f84a4a4bdb6d8994689e3d5267d
Armin Cetina Mac <ext.armin.cetina@pricetravel.com>
2023-07-24 21:26:49 -0500

    First commit

---
+++ Services/TokenService.cs
@@ -0,0 +1,29 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using Microsoft.IdentityModel.Tokens;
+using MinimalAPI.Security.Models;
+using MinimalAPI.Security.Settings;
+
+namespace MinimalAPI.Security.Services;
+
+internal class TokenService
+{
+    internal string GenerateToken(User user)
+    {
+        var tokenHandler = new JwtSecurityTokenHandler();
+        var key = Setting.GenerateSecretKey();
+        var tokenDescriptor = new SecurityTokenDescriptor()
+        {
+            Subject = new ClaimsIdentity(new Claim[]
+            {
+                new Claim(ClaimTypes.Name, user.Username),
+                new Claim(ClaimTypes.Role, user.Role)
+            }),
+            Expires = DateTime.UtcNow.AddMinutes(30),
+            SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+        };
+        return tokenHandler.WriteToken(
+                tokenHandler.CreateToken(tokenDescriptor)
+            );
+    }
+}
---
+++ Settings/Setting.cs
@@ -0,0 +1,9 @@
+﻿using System.Text;
+
+namespace MinimalAPI.Security.Settings;
+
+public static class Setting
+{
+    private static string SecretKey = "6ceccd7405ef4b00b2630009be568cfa";
+    public static byte[] GenerateSecretKey() => Encoding.ASCII.GetBytes(SecretKey);
+}
